f1af87658bfe573730374b7460ad6a8e115568a5
Aluno <Aluno@Digital.net>
2018-08-27 14:37:00 -0300

    Adicionei 2 exercicios

---
+++ soma de nome+nascimento/main.js
@@ -0,0 +1,13 @@
+
+    function converter (){
+
+        var ano = document.getElementById('ano').value;
+        var idade = 2018-ano;
+        document.getElementById('idade').innerHTML = "Voce tem "+idade+" Anos";
+
+
+        var nome= document.getElementById('nome').value;
+        document.getElementById('nome2').innerHTML = "Oi "+nome;
+    }
+    
+    
